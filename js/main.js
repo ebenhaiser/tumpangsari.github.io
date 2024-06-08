e783398fd4a3a99fb4c879df190cd184f86e0870
Ebenhaiser Caprisiano <ebenhaiser@gmail.com>
2024-06-09 03:31:22 +0700

    Pesan commit Anda di sini

---
+++ js/main.js
@@ -0,0 +1,266 @@
+(function($) {
+
+	'use strict';
+
+	// bootstrap dropdown hover
+
+  // loader
+  var loader = function() {
+    setTimeout(function() { 
+      if($('#loader').length > 0) {
+        $('#loader').removeClass('show');
+      }
+    }, 1);
+  };
+  loader();
+
+  // Stellar
+  $(window).stellar();
+
+	
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			$this.find('.dropdown-menu').removeClass('show');
+	});
+
+
+	$('#dropdown04').on('show.bs.dropdown', function () {
+	  console.log('show');
+	});
+
+
+
+	// home slider
+	$('.home-slider').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav:true,
+    autoplayHoverPause: true,
+    items: 1,
+    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:1,
+        nav:false
+      },
+      1000:{
+        items:1,
+        nav:true
+      }
+    }
+	});
+
+	// owl carousel
+	var majorCarousel = $('.js-carousel-1');
+	majorCarousel.owlCarousel({
+    loop:true,
+    autoplay: false,
+    stagePadding: 0,
+    margin: 10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: false,
+    dots: false,
+    autoplayHoverPause: false,
+    items: 3,
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:3,
+        nav:true,
+        loop:false
+      }
+  	}
+	});
+
+  // cusotm owl navigation events
+  $('.custom-next').click(function(event){
+    event.preventDefault();
+    // majorCarousel.trigger('owl.next');
+    majorCarousel.trigger('next.owl.carousel');
+
+  })
+  $('.custom-prev').click(function(event){
+    event.preventDefault();
+    // majorCarousel.trigger('owl.prev');
+    majorCarousel.trigger('prev.owl.carousel');
+  })
+
+	// owl carousel
+	var major2Carousel = $('.js-carousel-2');
+	major2Carousel.owlCarousel({
+    loop:true,
+    autoplay: true,
+    stagePadding: 7,
+    margin: 20,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: false,
+    autoplayHoverPause: true,
+    items: 4,
+    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:3,
+        nav:false
+      },
+      1000:{
+        items:4,
+        nav:true,
+        loop:false
+      }
+  	}
+	});
+
+
+// NAVBAR KETIKA SCROLL
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.element-animate').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('element-animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .element-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn element-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft element-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight element-animated');
+							} else {
+								el.addClass('fadeInUp element-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 100);
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '95%' } );
+	};
+	contentWayPoint();
+
+  window.addEventListener('scroll', function() {
+    var header = document.getElementById('header');
+
+    // Tambahkan atau hapus kelas 'header-scrolled' berdasarkan posisi scroll
+    if (window.scrollY > 50) {
+      header.classList.add('header-scrolled');
+    } else {
+      header.classList.remove('header-scrolled');
+    }
+  });
+  
+// NAVBAR JADI PUTIH KETIKA DI SECTION
+  document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('#navbar .nav-link');
+    
+    const handleScroll = () => {
+      const scrollPos = window.scrollY;
+      const isMobile = window.innerWidth <= 992;
+
+      navLinks.forEach(link => {
+        const section = document.querySelector(link.getAttribute('href'));
+        if (section) {
+          const sectionTop = section.offsetTop;
+          const sectionHeight = section.offsetHeight;
+
+          if (isMobile || scrollPos >= sectionTop - 10) {
+            if (scrollPos >= sectionTop - (isMobile ? 0 : 0) && scrollPos < sectionTop + sectionHeight) {
+              link.classList.add('active-section');
+            } else {
+              link.classList.remove('active-section');
+            }
+          } else {
+            link.classList.remove('active-section');
+          }
+        }
+      });
+    };
+
+    navLinks.forEach(link => {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        const isMobile = window.innerWidth <= 992;
+        const offset = isMobile ? 0 : -10;
+
+        if (section) {
+          window.scrollTo({
+            top: section.offsetTop - offset,
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Call on load in case user is not at the top
+  });
+
+
+  // SCROLL TO TOP BUTTON
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    var backButton = document.querySelector('.back-to-top');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      if (!backButton.classList.contains('show')) {
+        backButton.classList.add('show');
+        backButton.style.animation = 'none'; // Menghentikan animasi saat ditambahkan kelas 'show'
+        backButton.offsetHeight; // Memaksa browser memperbarui layout
+        backButton.style.animation = null; // Setel kembali animasi ke nilai awal (akan mulai lagi)
+      }
+    } else {
+      backButton.classList.remove('show');
+    }
+  }
+
+  document.querySelector('.back-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'  
+    });
+  });
+
+
+})(jQuery);
+
+
+
