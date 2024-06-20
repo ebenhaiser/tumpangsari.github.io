c8177cd5f16f09b0af746b1e9382b39db5926e2a
Ebenhaiser Caprisiano <ebenhaiser@gmail.com>
2024-06-21 03:32:59 +0700

    directory update

---
+++ js/data.js
@@ -0,0 +1,76 @@
+const roomsSection = {
+    room: [
+        {
+            id: 0,
+            name: "Guest House",
+            numberOfRoom: "4 rooms",
+            numberOfGuest: "1-2 Guests",
+            roomSize: "12 m",
+            description: "Nulla vel metus scelerisque ante sollicitudin. Fusce condimentum nunc ac nisi vulputate fringilla.",
+            images: [
+                {
+                    imageId: 0,
+                    imageActive: true,
+                    imagePath: "images/guestHouse/guestHouse1.jpg",
+                },
+                {
+                    imageId: 1,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse2.jpg"
+                },
+                {
+                    imageId: 2,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse3.jpg",
+                },
+                {
+                    imageId: 3,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse4.jpg",
+                },
+                {
+                    imageId: 4,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse5.jpg",
+                },
+                {
+                    imageId: 5,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse6.jpg",
+                },
+                {
+                    imageId: 6,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse7.jpg",
+                },
+                {
+                    imageId: 7,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse8.jpg",
+                },
+                {
+                    imageId: 8,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse9.jpg",
+                },
+                {
+                    imageId: 9,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse10.jpg",
+                },
+                {
+                    imageId: 10,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse11.jpg",
+                },
+                {
+                    imageId: 11,
+                    imageActive: false,
+                    imagePath: "images/guestHouse/guestHouse12.jpg",
+                },
+            ],
+            facilities: "Kitchen, Private Bathroom, Parking Lot, Internet, Security System, Central City Location",
+            location: "Gg. Kenari Indah I, Miliran UH 2, D.I. Yogyakarta",
+        }
+    ]
+}
